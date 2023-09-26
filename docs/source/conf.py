950dcb7a0e115933c0866458219ff27dce7a322a
browniano <plgm64@gmail.com>
2023-09-26 11:21:37 +0200

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 #project = 'python_R_environments'
-project = 'HPC'
+project = 'HPCdocs'
 copyright = '2023, Pablo L. García-Müller'
 author = 'Pablo L. García-Müller'
 
