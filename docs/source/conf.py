678ea5e184163801f7f42cd8b1f55bb8a110f6fe
browniano <plgm64@gmail.com>
2023-09-26 12:42:22 +0200

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 
 language = 'es'
@@ -46,5 +46,5 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'furo' #'alabaster'
-#html_static_path = ['_static']
+html_theme = 'furo' #'alabaster'
+html_static_path = ['_static']
