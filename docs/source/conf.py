f1ed4164c8d1693904d16660870f0262212b55e8
browniano <plgm64@gmail.com>
2023-09-26 12:31:45 +0200

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,18 +10,41 @@
 project = 'HPCdocs'
 copyright = '2023, Pablo L. García-Müller'
 author = 'Pablo L. García-Müller'
+release = '0.1'
+version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser'] #['sphinx_multitoc_numbering']
+#extensions = ['myst_parser'] #['sphinx_multitoc_numbering']
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.myst_parser',
+]
+
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
-exclude_patterns = []
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
 
 language = 'es'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo' #'alabaster'
-html_static_path = ['_static']
+#html_theme = 'furo' #'alabaster'
+#html_static_path = ['_static']
